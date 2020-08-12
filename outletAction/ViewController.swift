8a2fb8bac9b27f3495277e626d38150ab78290a7
twber <hctsai@umich.edu>
2020-08-12 13:58:49 +0800

    var  entryTex = UITextField !

--- outletAction/ViewController.swift
+++ outletAction/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var entryText: UITextField! 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func littleMouse(_ sender: Any) {
+        let speechUtterance = AVSpeechUtterance(string:entryText.text!)
+        speechUtterance.rate = 0.4
+        speechUtterance.pitchMultiplier = 1.5
+        speechUtterance.voice = AVSpeechSynthesisVoice(language:"zh-TW")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speechUtterance)
+    }
+    
 }
 
